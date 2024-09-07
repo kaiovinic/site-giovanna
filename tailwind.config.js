d88aea372d87580fe719207ef85167962d8b5e81
kaiovinic <kaiovini19@hotmail.com>
2024-09-07 00:13:35 -0300

    primeiro commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,8 +11,37 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "#473936",
+        },
+        secondary: {
+          DEFAULT: "#f2dfce",
+        },
+        accent: {
+          DEFAULT: "#f19687",
+          100: "#f2d5c5",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
+    container: {
+      center: true,
+      padding: "15px",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "960px",
+        xl: "1200px",
+      },
+    },
+    fontFamily: {
+      primary: "var(--font-marcellus)",
+      secondary: "var(--font-montserrat)",
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };

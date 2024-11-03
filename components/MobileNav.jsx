6ea67adc8004e55e6bdad203de881f6ea97e3c5b
kaiovinic <silvasantoskaiovinicius@gmail.com>
2024-11-03 00:08:12 -0300

    Update MobileNav.jsx

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -6,26 +6,15 @@ import { IoCloseOutline } from "react-icons/io5";
 import Socials from "./Socials";
 
 const links = [
-  {
-    href: "/",
-    name: "Home",
-  },
-  {
-    href: "/about",
-    name: "Sobre",
-  },
-  {
-    href: "/servicos",
-    name: "Serviços",
-  },
-  {
-    href: "/contact",
-    name: "Contato",
-  },
+  { href: "/", name: "Home" },
+  { href: "/about", name: "Sobre" },
+  { href: "/servicos", name: "Serviços" },
+  { href: "/contact", name: "Contato" },
 ];
 
 const MobileNav = ({ setMobileNav }) => {
   const pathname = usePathname();
+
   return (
     <nav className="relative flex flex-col justify-between h-full p-8">
       <div
@@ -35,19 +24,18 @@ const MobileNav = ({ setMobileNav }) => {
         <IoCloseOutline className="text-4xl" />
       </div>
       <ul className="flex flex-col gap-10 text-white text-xl">
-        {links.map((link, index) => {
-          return (
-            <Link
-              key={index}
-              href={link.href}
-              className={`${
-                pathname === link.href && "border-b-2 border-accent"
-              } uppercase max-w-max mx-auto`}
-            >
-              {link.name}
-            </Link>
-          );
-        })}
+        {links.map((link, index) => (
+          <Link
+            key={index}
+            href={link.href}
+            className={`${
+              pathname === link.href && "border-b-2 border-accent"
+            } uppercase max-w-max mx-auto`}
+            onClick={() => setMobileNav(false)} // Fecha o modal ao clicar
+          >
+            {link.name}
+          </Link>
+        ))}
       </ul>
       <Socials containerStyles="text-white text-lg flex gap-6 justify-center" />
     </nav>
